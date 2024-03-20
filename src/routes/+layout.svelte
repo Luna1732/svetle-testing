f3ef2440af7ca1eb224dc20adaf7be5926b04b22
Lunatic <Lunatic@mail.ru>
2024-03-20 21:33:28 +0500

    add blog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,11 @@
         <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
         <div class="nav-links">
             {#each nav as link}
+                 {#if link.isShow}
 
-            <a href={link.href} class="link">{link.title}</a>
-            
+                     <a href={link.href} class="link">{link.title}</a>
+           
+                 {/if}
             {/each}
         </div>
     </div>
@@ -18,25 +20,30 @@
     const nav = [
         {
             title: 'Обо мне',
-            href: '/about'
+            href: '/about',
+            isShow: true,
         },
         {
             title: 'Блог',
-            href: '/blog'
+            href: '/blog',
+            isShow: true,
         },
         {
             title: 'Портфолио',
-            href: '/project'
+            href: '/project',
+            isShow: true,
         },
         {
             title: 'Контакты',
-            href: '/contact'
+            href: '/contact',
+            isShow: true,
         },
         {
             title: 'temp',
-            href: '/temp'
-        }
-    ]
+            href: '/temp',
+            isShow: false,
+        },
+    ];
 </script>
 <style>
     .container{
