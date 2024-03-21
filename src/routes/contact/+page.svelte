9629c74927c26da2e154e59ded63393802d15b50
Lunatic <Lunatic@mail.ru>
2024-03-21 20:32:15 +0500

    test hostin3

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,7 +1,7 @@
 <svelte:head>
         <title>Контакты</title>
     </svelte:head>
-    <div class="container1">
+    <div class="container">
         <h1>    <span class="red">К</span>
             <span class="black">о</span>
             <span class="blue">н</span>
@@ -14,13 +14,13 @@
 
         <div class="social-buttons">
             <button class="telegram" onclick="window.location.href = 'https://t.me/premium';">
-                <img src="https://photos.google.com/photo/AF1QipN2jMZ4BgVelAPPJ8M3IypBI4K1E3w-EnkujX7g" alt="Telegram">
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczON7NBAASt59exI2B6FrhyE0Hjg1xbKQWZU0EG2sPLBVZj_GLwpwHoETJOe6L0gEvqIBmtPvTpaEXG_In2o94yUZJWIHNnJiIyZQXjCrcANJusdJCAdlgMP8wu_gMgM39hfqC0rAOLXHDDe-k84_R49yqAMQVyJuy1X7p9kesBrWZFFvCizxZxc95_mAOq_6OQwZyuujiHmUk7mPYAcotvUXUY1fcIg416XfaLnMRIo_qi1FkuewEy3IeF6kz5OxH-2rEi1gLaFtmmzPYfMlZ_3KEe_xc51kwp07zrnsO7uWD-e_k3h9lFnaSqDpS-pHnxW_0SnQTTJH6PpvMb9wlk7dfJty3nQ_a3JPmX0JaHxv7SSrJ07LabwfWild1M-fhP22zfPTiWDiXZ4f9tV5bf0TsDBdnGf3QWwrMKbvnYo23WTV3YSv4JPXfRDtKfQVyDermB1c8X9On9TRjTHBGrX6MPgef317wUIR00Kqb1g84_YC43JAj9egiaMfHqt8-244xGp1hg-rA6KhPFDcthbRfZo6ucXbnwNN8ENDdZcZl2MmEe2rItnZz4ReZKQocl26xXby4O_gaXnGK3s6gP_68oHoDRTtSr1fU0Ez46m3wL64oYsR0LBXeJmQQl-qdbTIUpi1Jx7rpxvl-rr32_v8bBR7Otn_ilA1-vKPoE0jPVL2aUnu-6lP_UxErXa7UiVE_W793qCT-L-wadTpyicvo3gqBe_2spDExrEIlT-g80798_YYZkuT-dkswr-RoqUCYBkMzgrKhdGXlzWKyOEBXHmY387xi5K5XBLZkVEeWXdBkNclB-DwyMJ-CkkUCNmwzWrglELcArxoGxRAWdAVaq17O9LbMoIr-6F5UCAbLsgG9VwpvnS862MybDxlv4HTPtj8Rc7fG1d6xKtMkk=w1542-h964-s-no-gm?authuser=0" alt="Telegram">
             </button>
             <button class="whatsapp" onclick="window.location.href = 'https://wa.me/+79434314511';">
-                <img src="/src/lib/Images/whatsapp-png-whatsapp-logo-vetor-png-5000.png" alt="WhatsApp">
+                <img src="https://photos.google.com/photo/AF1QipPFTcD3M9jV7iK1bKCy8ZsWSloAn10L_IpICT5Q" alt="WhatsApp">
             </button>
             <button class="vk" onclick="window.location.href = 'https://www.4chan.org/?ref=infernosites';">
-                <img src="/src/lib/Images/vk-4096.png" alt="VKontakte">
+                <img src="https://photos.google.com/photo/AF1QipMT0ucGw_PtgCx10kZ1a1mcHcHfxBuWM9qX8IiA" alt="VKontakte">
             </button>
         </div>
     </div>
@@ -42,7 +42,7 @@
             width: 400px; /* Размер логотипа */
         }
     
-        .container1 {
+        .container {
             background: url(https://photos.app.goo.gl/ujRQBzWpH5hKG1gD9) center center/cover no-repeat;
     }
       
