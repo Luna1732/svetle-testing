88e2f8f632dde6f949c63d7d106a0841413e3d74
Lunatic <Lunatic@mail.ru>
2024-03-21 14:10:25 +0500

    add 1

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,29 @@
-<h1>Обо мне</h1>
- 
+<script>
 
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatibus eum deleniti iure, inventore reprehenderit rerum autem vero, officiis minus nihil ipsa, saepe repellendus expedita blanditiis qui! Rerum, dignissimos maiores.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident magni praesentium sint nostrum distinctio reprehenderit quam libero, repellat molestias amet laboriosam reiciendis quia. Eos, facilis. Sequi quos cumque blanditiis architecto?
+  import { backIn } from "svelte/easing";
+
+</script>
+<svelte:head>
+        <title>Обо Мне</title>
+    </svelte:head>
+    <div class="container">
+        <h1>Обо Мне</h1>
+        
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatibus eum deleniti iure, inventore reprehenderit rerum autem vero, officiis minus nihil ipsa, saepe repellendus expedita blanditiis qui! Rerum, dignissimos maiores.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident magni praesentium sint nostrum distinctio reprehenderit quam libero, repellat molestias amet laboriosam reiciendis quia. Eos, facilis. Sequi quos cumque blanditiis architecto?
+        
+        <p1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam nulla accusamus totam voluptas officiis odio cupiditate non quia cumque. Possimus, esse suscipit accusantium nulla repellendus exercitationem est mollitia quod perferendis fugiat natus iure libero vel rerum, quam beatae officiis ratione quia eius nisi delectus hic cum. Est numquam optio placeat!</p1>
     
-    <p1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam nulla accusamus totam voluptas officiis odio cupiditate non quia cumque. Possimus, esse suscipit accusantium nulla repellendus exercitationem est mollitia quod perferendis fugiat natus iure libero vel rerum, quam beatae officiis ratione quia eius nisi delectus hic cum. Est numquam optio placeat!</p1>
+        <div class="back">
+            <img src="/src/lib/Images/8cNQy_LboMc.jpg" alt="eGO NET" width="1000">
+        </div>
 
+        <style>
+            .back{
+                text-align: center;
+            }
+        </style>
+    
+    </div>
 
+   
