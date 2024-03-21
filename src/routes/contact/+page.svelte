eded42a16bbdb1c8bb3b16a4809347b5d80dc68c
Lunatic <Lunatic@mail.ru>
2024-03-21 20:37:05 +0500

    последний тест

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,8 +1,9 @@
 <svelte:head>
         <title>Контакты</title>
     </svelte:head>
-    <div class="container">
-        <h1>    <span class="red">К</span>
+    <div class="container1">
+        <h1>    
+            <span class="red">К</span>
             <span class="black">о</span>
             <span class="blue">н</span>
             <span class="green">т</span>
@@ -17,10 +18,10 @@
                 <img src="https://lh3.googleusercontent.com/pw/AP1GczON7NBAASt59exI2B6FrhyE0Hjg1xbKQWZU0EG2sPLBVZj_GLwpwHoETJOe6L0gEvqIBmtPvTpaEXG_In2o94yUZJWIHNnJiIyZQXjCrcANJusdJCAdlgMP8wu_gMgM39hfqC0rAOLXHDDe-k84_R49yqAMQVyJuy1X7p9kesBrWZFFvCizxZxc95_mAOq_6OQwZyuujiHmUk7mPYAcotvUXUY1fcIg416XfaLnMRIo_qi1FkuewEy3IeF6kz5OxH-2rEi1gLaFtmmzPYfMlZ_3KEe_xc51kwp07zrnsO7uWD-e_k3h9lFnaSqDpS-pHnxW_0SnQTTJH6PpvMb9wlk7dfJty3nQ_a3JPmX0JaHxv7SSrJ07LabwfWild1M-fhP22zfPTiWDiXZ4f9tV5bf0TsDBdnGf3QWwrMKbvnYo23WTV3YSv4JPXfRDtKfQVyDermB1c8X9On9TRjTHBGrX6MPgef317wUIR00Kqb1g84_YC43JAj9egiaMfHqt8-244xGp1hg-rA6KhPFDcthbRfZo6ucXbnwNN8ENDdZcZl2MmEe2rItnZz4ReZKQocl26xXby4O_gaXnGK3s6gP_68oHoDRTtSr1fU0Ez46m3wL64oYsR0LBXeJmQQl-qdbTIUpi1Jx7rpxvl-rr32_v8bBR7Otn_ilA1-vKPoE0jPVL2aUnu-6lP_UxErXa7UiVE_W793qCT-L-wadTpyicvo3gqBe_2spDExrEIlT-g80798_YYZkuT-dkswr-RoqUCYBkMzgrKhdGXlzWKyOEBXHmY387xi5K5XBLZkVEeWXdBkNclB-DwyMJ-CkkUCNmwzWrglELcArxoGxRAWdAVaq17O9LbMoIr-6F5UCAbLsgG9VwpvnS862MybDxlv4HTPtj8Rc7fG1d6xKtMkk=w1542-h964-s-no-gm?authuser=0" alt="Telegram">
             </button>
             <button class="whatsapp" onclick="window.location.href = 'https://wa.me/+79434314511';">
-                <img src="https://photos.google.com/photo/AF1QipPFTcD3M9jV7iK1bKCy8ZsWSloAn10L_IpICT5Q" alt="WhatsApp">
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczOPQASZFLvD80Q6ipJeDKnp0yuR0F4f-XA2CV0HDLsE9FXIQfohd8v6NUJ5b7fpT1QMrhrI8t5XzAYfPkA491tZ3sC8MwXQWt5FnJQM_13qCjGCmwX27r_UN3JyR8_uvd9SVGo80yA8PoEUiUwIqGr9-sSnNGJRz3B14VDUmBJPkcdLntx4no_LbjXd5HXZi1TKn4EG9Me1_Ud95poEepRF30-7HuhotD7S2Top_DsGV4HngZGdPBTFPvVrjyiplViaxxqF5Jjk_YbHFPD9LuDNs97Uw1dJJt7-NvlY8jk5xWjaNQ-1Mk-hNdDaDwsngdogj_tFxz8UBSdMmovR3oD6boNoZpSrHsxTC1K7uFrFLWsPvQwVK8TxBkE70pCii-HZzpZEedfiUYmWpYFvKFrWiUEidEa5_uovoFUFiIPnVGOsF_pWNH7iiJfqOv-UPrfEjjQlVZ7rQXE-e9d7wsMAc0YdzudecvNHw5sYbZvqIQnWIR0s0wMqifyqM4E4-XUSdaxZsxUb4MQPacvcyBH3sPvwXcsbIAEy6z0VB2qXn8VEDySxh8tJbsaxsQaqkG-4uEiAXHEeafdjgtWXucu1ZE-o9UIQYmHi87jfQyxHeYLxyxI6YjncR985zQGwkgr2HO4nXTL1UwzfxzjDbWk2wdg9rcsq9N-drUwWcq5bqEbU2500nhYuquNGrWDnu6gy0Net_ybj_9XW7i8K0yQucwvDsIfS9Qvvv_Pu3r8SiHwVAH0_QYzGajsKZikUMIZim3evfuN2cccewlw-jEYatoakeiEwvkR9CiRfwstTp9AWwgGk9kuiDO5UVlfYbsTGcNxAJkT2NoEyg5ed2RL250paKnodaiHbT9bXQ8gU6-MGYr3okd07MO3ogvqoqrUw090KhTVawCmAZZ8Z8nk=w1359-h964-s-no-gm?authuser=0" alt="WhatsApp">
             </button>
             <button class="vk" onclick="window.location.href = 'https://www.4chan.org/?ref=infernosites';">
-                <img src="https://photos.google.com/photo/AF1QipMT0ucGw_PtgCx10kZ1a1mcHcHfxBuWM9qX8IiA" alt="VKontakte">
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczMrvTJFb_OsLeNgEvryOXGpsnAYjlIMUxFoDKc6ISHj8uBu0Kblx1G_mtV7JQvvQ8kdEwi31mJsvjuhXM5Kixq5gyM1-P-GJxPE85xSdQMIpUr3G3hp08K_n69bu5UgC3o2uwIBrG4bXO108Ann3s3N-3bhSP-dcYMjTmV443OSlDU2B8v5mQv33cNxHkWRGjWlimSmSiBLUEGlWHJfNhJHq7aVf87LhVoXNpOf8s0EX7vHdjARQLz20RiQn3xrOBQRqXqe0uzqEbOzNpDyR2DvlsQQKL0c8cZumL7kogmNgSI7xazpoT-WqXfyDgbVAz9fpUlX09ADRPViMyOYs27_277KZOmUThcBpSD5cpUBPICSIteslSFrPLKfge12Ie_heXJRoWs98LVIyW6E47Z8QVW4bSOonCBE1m5iEnsBzCzJ7l8cH3FlLkbjzIWMWkpb88ylDJaFgJbnKeDJR77genLOAl6urCri01huWLMqj8BpZT_bSb-cFuighakzbB5wu40VZ5m02MQ2S8kx908UUEJra2SbwZdjMnW8Mi_bYRvbD44JkCmNSDeesKY8_ByBaHmmd8QMjHCUPRPGi_AW6PUKo3E1Oy8SP_-xSZyXT375aH8UEhYGdPlygz9gbIDkidNvheDWAQU1pQRvs2FUhR9kEJbFHh1CdorSxq6EIQVL8Z1JbdHvYZbLlvwhizy0FPqN1dZRqYt8ncEXHco0Zj03k0JPEI1FVkgbQ8qdZGiTlR8qlHNGAI0_fGcWIfFCNAHSS3M6oW86tiqt3Ig_24nfevyXpoGYWyev2SLcks9__U23LXXQ4DT2uOnmo4t3cxfJhduOkGMMV_skJczr0XqCov7T4SxH5SV93XThw08fjZVYJmxQvmmo2Muna55Zzc5CvyUlVPajv_bE3Xc=w964-h964-s-no-gm?authuser=0" alt="VKontakte">
             </button>
         </div>
     </div>
@@ -42,7 +43,7 @@
             width: 400px; /* Размер логотипа */
         }
     
-        .container {
+        .container1 {
             background: url(https://photos.app.goo.gl/ujRQBzWpH5hKG1gD9) center center/cover no-repeat;
     }
       
